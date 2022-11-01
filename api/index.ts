b6ae623a8d935431903ba505d741607fbc43ffde
shfshanyue <xianger94@gmail.com>
2022-11-01 10:32:59 +0800

    chore: http cookies

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import Koa from 'koa'
 import Router from 'koa-router'
 import koaBody from 'koa-body'
+import cookie from 'cookie'
+import * as _ from 'midash'
 
 const router = new Router()
 const app = new Koa()
@@ -9,7 +11,7 @@ router.get('/api', ctx => {
   ctx.body = 'hello, world'
 })
 
-router.get('/api/cache-control', ctx => {
+router.get('/api/cache', ctx => {
   const { cacheControl, age } = ctx.query
   if (cacheControl) {
     ctx.set('Cache-Control', cacheControl)
@@ -21,10 +23,30 @@ router.get('/api/cache-control', ctx => {
   ctx.body = headers
 })
 
-router.get('/api/cache-control/:duration', ctx => {
+router.get('/api/cache/:duration', ctx => {
   ctx.set('Cache-Control', `max-age=${ctx.params.duration}`) 
   const headers = ctx.res.getHeaders()
   ctx.body = headers
+})
+
+router.get('/api/cookies', ctx => {
+  const cookies = cookie.parse(ctx.req.headers.cookie || '')
+  ctx.body = cookies || {}
+})
+
+router.get('/api/cookies/set/:key/:value', ctx => {
+  const { key, value } = ctx.params
+  ctx.cookies.set(key, value, {
+    ..._.pick(ctx.query, [
+      'maxAge',
+      'path',
+      'domain'
+    ]) as any,
+    maxAge: Number(ctx.query.maxAge || 0) * 1000,
+    httpOnly: ctx.query.httpOnly === 'true',
+    secure: Boolean(ctx.query.secure)
+  })
+  ctx.redirect('/api/cookies')
 })
 
 app.use(koaBody())
